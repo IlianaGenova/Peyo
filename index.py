ece518297fab2e590353b59b78b916524298f62c
Iliana Genova <iliana.genova.02@gmail.com>
2020-10-03 20:55:25 +0300

    interconnecting/routing buttons to functions

--- index.py
+++ index.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/radio')
+def playRadio():
+
+    return render_template('index.html')
+
+@app.route('/spotify')
+def playSpotify():
+	
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
