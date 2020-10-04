41b021b4bdf542d2d41183dd701926e468b5cf4b
Venelin Atanasov <venelin@pop-os.localdomain>
2020-10-04 16:27:19 +0300

    works, seems stable

--- usb.py
+++ usb.py
@@ -1,16 +1,22 @@
 import os
 
+
+
+
+
 def play_usb():
     ls=os.listdir('/media/pi')
+    
     dir='/media/pi/'+ ls[0]
-   
-    os.chdir('/media/pi/flash')
-    os.system('for a in *; do omxplayer "$a"; done')
+    
     print(dir)
+    #os.chdir(dir)
+    for i in os.listdir(dir):
+        if(str(i)=='.Trash-1000'):
+            continue
 
-
+        print('omxplayer {}'.format( '"' + "'"+ str(dir)+'/' + str(i) + "'" + '"'))
+        os.system('omxplayer {}'.format(str(dir)+'/' + str(i)))
 if __name__ == "__main__":
-
     play_usb()
-
-    pass 
+    pass
