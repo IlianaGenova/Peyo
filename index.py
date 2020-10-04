c874f18cdcfc2b2ece26b1a58682682ccc0c1c1f
Iliana Genova <iliana.genova.02@gmail.com>
2020-10-04 10:23:55 +0300

    volume and backend-frontend interconnection progress

--- index.py
+++ index.py
@@ -1,19 +1,38 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
+
+softwareOn = False;
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/radio')
 def playRadio():
-
-    return render_template('index.html')
+	print("radio is now playing");
+	return '', 200;
 
 @app.route('/spotify')
 def playSpotify():
-	
-    return render_template('index.html')
+
+	return render_template('index.html')
+
+@app.route('/software-mode')
+def controlBySoftware():
+	softwareOn = True
+	return '', 200;
+
+@app.route('/volume', methods=['POST', 'GET'])
+def controlVolume():
+	if request.method == 'POST':
+		volume = request.form['myRange']
+		if(softwareOn):
+			#logic
+			# print(volume)
+			print('yay')
+	elif request.method == 'GET':
+		print('get')
+	return '', 200;
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
