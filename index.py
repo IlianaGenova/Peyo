ea51ddee9a368505ebd89f4eba8b0db8e1ae55ac
Iliana Genova <iliana.genova.02@gmail.com>
2020-10-04 12:01:28 +0300

    toggle button connected with backend

--- index.py
+++ index.py
@@ -1,37 +1,48 @@
 from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 
-softwareOn = False;
+softwareOn = 0;
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/radio')
+@app.route('/radio', methods = ['POST', 'GET'])
 def playRadio():
 	print("radio is now playing");
 	return '', 200;
 
-@app.route('/spotify')
+@app.route('/spotify', methods  =['POST', 'GET'])
 def playSpotify():
 
 	return render_template('index.html')
 
-@app.route('/software-mode')
+@app.route('/mode', methods = ['POST', 'GET'])
 def controlBySoftware():
-	softwareOn = True
+	if request.method == 'POST':
+		global softwareOn;
+		state = request.form['toggleBar']
+		if(state == "true"):
+			softwareOn = 1
+		elif(state == "false"):
+			softwareOn = 0
+		print(softwareOn)
+		print(state)
 	return '', 200;
 
-@app.route('/volume', methods=['POST', 'GET'])
+@app.route('/volume', methods = ['POST', 'GET'])
 def controlVolume():
 	if request.method == 'POST':
 		volume = request.form['myRange']
+		# print("ti i si ma " + str(softwareOn))
 		if(softwareOn):
 			#logic
-			# print(volume)
 			print('yay')
-	elif request.method == 'GET':
-		print('get')
+		else:
+			return jsonify({'softwareOn' : softwareOn})
+		return jsonify({'softwareOn' : softwareOn})
+	# elif request.method == 'GET':
+	# 	print('get')
 	return '', 200;
 
 if __name__ == '__main__':
