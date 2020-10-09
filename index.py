0412f09ba0f48d3f5c6354bff2a05b2cfa4b10fa
venelinatanasov <venelin.atanasov2@gmail.com>
2020-10-09 21:15:01 +0300

    A minor change, coordinated with the HackTues team.

--- index.py
+++ index.py
@@ -149,7 +149,7 @@ def controlStartStop():
 			else:
 				print("else" + str(current_volume))
 				if radio_i:
-					value = float(current_volume/100)
+					value = float(int(current_volume)/100)
 					system('''export DBUS_SESSION_BUS_ADDRESS=$(cat /tmp/omxplayerdbus.${USER:-root})
 						dbus-send --print-reply --session --reply-timeout=500 --dest=org.mpris.MediaPlayer2.omxplayer /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Set string:"org.mpris.MediaPlayer2.Player" string:"Volume" double:''' + str(value))
 				if spotify_i:
